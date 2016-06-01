cca2705cc23edcd5d879a5745ba5ba7aeae2835b
shinkw <blackindigocrimson@gmail.com>
2016-06-01 18:32:39 +0900

    changed to dynamic list [TODO] Debug void print_expr.

--- type.h
+++ type.h
@@ -18,7 +18,7 @@
 #define OPERATION   (2)
 #define CONS        (3)
 //-------------//
-#define MAX_LIST (100)
+#define MAX_LIST    (100)
 
 typedef struct SExpression {
     /* Type of S-Expression.
@@ -44,47 +44,13 @@ typedef struct SExpression {
      *  (+ 1 (+ 2 3))
      *   | OPERATION:[0]:"+"
      *   | NUMBER:   [1]:"1"
-     *   | CONS:     [2]:redix
-     *        | OPERATION:[2]->_next[0]:"+"
-     *        | NUMBER:   [2]->_next[1]:"2"
-     *        | NUMBER   :[2]->_next[2]:"3"
+     *   | CONS:     [2]:| OPERATION:[2]->_next[0]:"+"
+     *                   | NUMBER:   [2]->_next[1]:"2"
+     *                   | NUMBER:   [2]->_next[2]:"3"
      */
 
     struct SExpression ** _next;
 
 } SExpr;
-
-void
-print_expr (SExpr * expr)
-{
-    if (expr->_type == NUMBER) {
-        printf("%s", expr->_data);
-    }
-    if (expr->_type == OPERATION) {
-        printf("%s", expr->_data);
-    }
-
-    if (expr->_type != NIL){
-        int i = 1;
-        for (i; i < MAX_LIST; i++){
-            switch (expr[i]._type){
-                case NIL:
-                    break;
-                case NUMBER:
-                    printf("%s", expr[i]._type);
-                    break;
-                case OPERATION:
-                    printf("%s", expr[i]._type);
-                    break;
-                case CONS:
-                    printf("(");
-                    print_expr(&expr[i]);
-                    break;
-                default:
-                    printf(")");
-            }
-        }
-    }
-}
 
 #endif // _TYPE_H
