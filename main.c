3286a73ae8c7baa977e52806bd2f9b48757b4539
shinkw <blackindigocrimson@gmail.com>
2016-07-02 21:15:38 +0900

    created parser

--- main.c
+++ main.c
@@ -15,12 +15,13 @@
 /**********************************************
                   TYPES
  *********************************************/
+#define MAX_FUNC_NAME 10
+
 enum {
   tNIL = 0,
   tNUM,
-  tSYG,
+  tSYM,
   tCONS,
-  tPRM,
   tFUN,
   tTRUE,
   tFalse,
@@ -29,11 +30,9 @@ enum {
 typedef struct ATOM {
   union {
     int value;
-    char symbol[1];
-    char *fn; /* PRIMITIVE */
+    char symbol[MAX_FUNC_NAME];
   };
 } ATOM;
-
 typedef struct CONS {
   int type;
 
@@ -42,10 +41,17 @@ typedef struct CONS {
   void *cdr;
   
 } CONS;
+typedef struct ENV {
+  char name[MAX_FUNC_NAME];
+  struct ENV *next;
+} ENV;
+
+const char symbol_chars[] = "~!@#$%^&*-_=+:/?<>";
 
 static CONS *NIL   = &(CONS){tNIL};
 static CONS *TRUE  = &(CONS){tTRUE};
 static CONS *False = &(CONS){tFalse};
+
 /**********************************************
      Memory manegement
  *********************************************/
@@ -66,32 +72,78 @@ static ATOM *makeNumber (int _value)
 }
 static ATOM *makeSYMBOL (char *_name)
 {
-  ATOM *atm = (ATOM *)malloc(sizeof(char));
-  strcpy(atm->symbol , _name);
-  return atm;
-}
-static ATOM *makeFUNC (char *_name)
-{
   ATOM *atm = (ATOM *)malloc(sizeof(char*));
-  strcpy(atm->fn , _name);
+  strcpy(atm->symbol, _name);
   return atm;
 }
-
 static int readNumber (char *_str)
 {
   int i = 0;
   int out = 0;
-  while(isdigit(_str[i]) || _str[i] != ')'){
+  while(0 <= (_str[i]-'0') && (_str[i]-'0') <= 9){
     out = out*10 + (_str[i]-'0');
     ++i;
   }
   return out;
 }
+static char *readSYMBOL (char *_str)
+{
+  int i = 0;
+  char *r = (char *)malloc(sizeof(char *));
+  while(_str[i] != ' ' && _str[i] != ')' && _str[i]){
+    r[i] = r[i] + _str[i];
+    i++;
+  }
+  return r;
+}
+
+static void printCons (CONS *cons, int nest)
+{
+  if (cons == NIL){
+    printf("\n");
+    return;
+  }else
+    
+  if (cons->type == tNUM){
+    printf("%*s%d::tNUM::%d\n",nest,"",nest,((ATOM *)cons->car)->value);
+    printCons((CONS *)(cons->cdr) , nest);
+  }else
+  if (cons->type == tSYM){
+    printf("%*s%d::tSYM::%s\n",nest,"",nest,((ATOM *)cons->car)->symbol);
+    printCons((CONS *)(cons->cdr) , nest);
+  }else
+  if (cons->type == tCONS){
+    printCons((CONS *)(cons->car), nest + 1);
+    printCons((CONS *)(cons->cdr), nest);
+  }
+  
+}
+/**********************************************
+           Env
+ *********************************************/
+static int find (char *key, ENV *_env)
+{
+  ENV *e = NULL;
+  for (e = _env; e != NULL; e = e->next){
+    if (strcmp(e->name , key) == 0){
+      return 1;
+    }
+  }
+  return 0;
+}
+static ENV *addEnv (char *_name , ENV *_root)
+{
+  ENV *e = (ENV *)malloc(sizeof(ENV *));
+  strcpy(e->name , _name);
+  e->next = _root;
+  return e;
+}
+
 /**********************************************
         Parser
  *********************************************/
 
-CONS *newCons (void *_car, void *_cdr , int _type)
+static CONS *newCons (void *_car, void *_cdr , int _type)
 {
   CONS *new = alloc(_type);
   new->car = _car;
@@ -99,42 +151,81 @@ CONS *newCons (void *_car, void *_cdr , int _type)
   return new;
 }
 
-CONS **parse (char *str){
-  CONS *ret = NULL;
-  int i = 0;
-  while (str[i++] != NULL){
-    
+static CONS *nReverse (CONS *_cons)
+{
+  CONS *ret = NIL;
+  while (_cons != NIL ){
+    CONS *tmp = _cons;
+    _cons = (CONS *)(_cons->cdr);
+    tmp->cdr = ret;
+    ret = tmp;
   }
+  return ret;
+}
+static int waitBrackets (char *str)
+{
+  int i = 0;
+  int leftCnt  = 1;
+  int rightCnt = 0;
+  while (leftCnt != rightCnt){
+    if (str[i] == '('){ leftCnt++;  }
+    if (str[i] == ')'){ rightCnt++; }
+    ++i;
+  }
+  return i;
 }
 
-char **tokenize (char *_str)
-{
-  char separater[] = " ";
-  char **out = NULL;
-  char *tok = strtok(_str, separater);
+static CONS *parse (char *str , ENV *_env){
+  CONS *ret = NIL;
   int i = 0;
-  while(tok != NULL){
-    out[i] = (char *)malloc(sizeof(char *));
-    strcpy(out[i],tok);
-    ++i;
-    tok = strtok(NULL,separater);
+  while (str[i] != '\0' && str[i]){
+    /*-----------------------------------------*/
+    if (str[i] == ' ') { /* ignore space , ¥n */
+      ++i;
+      continue;
+    /*-----------------------------------------*/
+    }else if (str[i] == '(') { /* init S-Exprs */
+      ret = newCons( parse(&str[i + 1], _env),
+                     ret,
+                     tCONS);
+      i += waitBrackets(&str[i+1]);
+    /*----------------------------------------*/
+    }else if (str[i] == ')') { /* end S-Exprs */
+      return nReverse(ret);
+    /*----------------------------------------------------*/
+    }else if (isdigit(str[i])) { /* make S-Expr of Number */
+      ret = newCons( makeNumber(readNumber(&str[i])),
+                     ret,
+                     tNUM);
+      while(str[i] != ' ' && str[i] != ')' && str[i]){i++;}
+      if (str[i] == ')'){break;}
+    /*---------------------------------------------------------------------------------*/
+    }else if (isalpha(str[i]) || strchr(symbol_chars,str[i])){/* make S-Expr of Symbol */
+      ret = newCons( makeSYMBOL(readSYMBOL(&str[i])),
+                     ret,
+                     tSYM);
+      while (str[i] != ' ' && str[i] != ')' && str[i]){i++;}
+      if (str[i] == ')'){break;}
+    }
+    /*--------------------------------------------*/
+    i++;
   }
-  return out;
+  return nReverse(ret);
 }
+
 
 /**********************************************
              Main Loop
  *********************************************/
 int main (void)
 {
+  ENV *allEnv = NULL;
   
-  char *str = "1234 a a 5";
-  char ** tokens = tokenize(str);
+  char str[] = "(defn (fn x) (+ 1 2 (* x 10) 100))";
 
-  int i = 0;
-  while(tokens[i] != NULL){
-    printf("%s\n",tokens[i]);
-  }
+  CONS *root = parse(str, allEnv);
+
+  printCons(root,0);
   
   return 0;
 }
