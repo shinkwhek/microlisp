543a28e61442034a343bd9224a2ca76c0ab58bb4
shinkw <blackindigocrimson@gmail.com>
2016-08-03 12:56:17 +0900

    def S-Expr

--- lisp.c
+++ lisp.c
@@ -14,7 +14,28 @@
 /**********************************************
                TYPES
  *********************************************/
+const int MAX_FUNC_NAME = 10;
 
+enum {
+  tNIL = 0,
+  tNUM,
+  tSYM,
+  tFUN,
+  tCONS
+};
+
+typedef struct SExpr {
+  int type;
+  void *car;
+  void *cdr;
+} SExpr;
+
+static SExpr *NIL = &(SExpr){tNIL};
+
+#define getCarAsInt(_expr)  ((int*)_expr->car)
+#define getCarAsChar(_expr) ((char*)_expr->car)
+#define getCarAsCons(_expr) ((SExpr*)_expr->car)
+#define getCdrAsCons(_expr) ((SExpr*)_expr->cdr)
 /**********************************************
                Memory manegement
  *********************************************/
