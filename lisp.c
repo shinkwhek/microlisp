37267765016ea5aa0d03d00810cd706af4424811
shinkw <blackindigocrimson@gmail.com>
2016-08-03 13:40:38 +0900

    Add parser

--- lisp.c
+++ lisp.c
@@ -15,6 +15,7 @@
                TYPES
  *********************************************/
 const int MAX_FUNC_NAME = 10;
+const char symbol_chars[] = "!%^*-=+/<>";
 
 enum {
   tNIL = 0,
@@ -47,6 +48,13 @@ static SExpr *alloc (int _typeName)
   _cons->car = NULL;
   _cons->cdr = NULL;
   return _cons;
+}
+static SExpr *newCons (void *_car , void *_cdr)
+{
+  SExpr *new = alloc(tCONS);
+  new->car = _car;
+  new->cdr = _cdr;
+  return new;
 }
 static SExpr *newNUM (int _value , void *_cdr)
 {
@@ -96,11 +104,104 @@ static char *readCharToken (char *_str)
 /**********************************************
                Parser
  *********************************************/
-
+static SExpr *nReverse (SExpr *_expr)
+{
+   CONS *ret = NIL;
+  while (_expr != NIL ){
+    CONS *tmp = _expr;
+    _expr = getCdrAsCons(_expr);
+    tmp->cdr = ret;
+    ret = tmp;
+  }
+  return ret;
+}
+static int waitBrackets (char *str)
+{
+  int i = 0;
+  int leftCnt  = 1;
+  int rightCnt = 0;
+  while (leftCnt != rightCnt){
+    if (str[i] == '('){ leftCnt++;  }
+    if (str[i] == ')'){ rightCnt++; }
+    ++i;
+  }
+  return i;
+}
+static SExpr *parse (char *str)
+{
+  SExpr *ret = NIL;
+  int i = 0;
+  while (str[i] != '\0' && str[i] != EOF){
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    if (str[i] == ' '){ /* Ignore space */
+      ++i;
+      continue;
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    }else if ( str[i] == '('){ /* Init S-Expr */
+      ret = newCons( parse(&str[i+1]),
+                     ret);
+      i = i + waitBrackets(&str[i+1]);
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    }else if ( str[i] == ')'){ /* End S-Exprs */
+      return nReverse(ret);
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    }else if ( isdigit(str[i])){ /* Make S-Expr of Number */
+      ret = newNUM( readChar2Int(&str[i]),
+                    ret);
+      while( str[i] != ' ' && str[i] != ')' && str[i]){i++;}
+      if (str[i] == ')'){break;}
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    }else if ( isalpha(str[i]) && strchr(symbol_chars,str[i]) ){/* Make S-Expr of Symbol */
+      ret = newSYM ( readCharToken(&str[i]),
+                     ret);
+      while(str[i] != ' ' && str[i] != ')' && str[i]){i++;}
+      if (str[i] == ')'){break;}
+      /* ---- ---- ---- ---- ---- ---- ----*/
+    }else if ( isalpha(str[i]) ){ /* Make S-Expr of Function */
+      ret = newFUN ( readCharToken(&str[i]),
+                     ret);
+      while(str[i] != ' ' && str[i] != ')' && str[i]){i++;}
+      if (str[i] == ')'){break;}
+    /* ---- ---- ---- ---- ---- ---- ---- */
+    }
+    i++;
+  }
+  return nReverse(ret);
+}
 /**********************************************
                Eval
  *********************************************/
 
+/**********************************************
+              Debug
+ *********************************************/
+static void printCons (CONS *cons, int nest)
+{
+  /*--------------type NIL----------------*/
+  if (cons == NIL || !cons){
+    printf("%*s%d::NIL \n",nest,"",nest);
+    return;
+  /*-------------type NUMBER--------------*/
+  }else if (cons->type == tNUM){
+    printf("%*s%d::tNUM::%d\n",nest,"",nest, getCarAsInt(cons));
+    printCons( getCdrAsConsCell(cons) , nest);
+  /*-------------type SYMBOL--------------*/
+  }else if (cons->type == tSYM){
+    printf("%*s%d::tSYM::%s\n",nest,"",nest, getCarAsChar(cons));
+    printCons( getCdrAsConsCell(cons) , nest);
+  /*-------------type Function------------*/
+  }else if (cons->type == tFUN){
+    printf("%*s%d::tFUN::%s\n",nest,"",nest, getCarAsChar(cons));
+    printCons( getCdrAsCons(cons) , nest);
+  /*------------type Cons Cell------------*/
+  }else if (cons->type == tCONS){
+    printCons( getCarAsCons(cons), nest + 1);
+    printCons( getCdrAsCons(cons), nest);
+  /*------------type NOT Know-------------*/
+  }else{
+    printf("i dont know.\n");
+  }
+}
 /**********************************************
                Main Loop
  *********************************************/
