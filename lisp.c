20863884f1c6d3c911bf70bf5c3a2bc406036b45
shinkwhek <klklpshin@gmail.com>
2017-05-20 13:42:32 +0900

    [ADD] comments

--- lisp.c
+++ lisp.c
@@ -4,7 +4,7 @@
 #include<string.h>
 
 FILE * fp;
-int c;
+int c;		// current
 
 /* ==== ==== ==== type ==== ==== ==== */
 enum {
@@ -40,24 +40,23 @@ static const char symbols[] = "+-*/!?=<>_:\\%#~&";
 
 /* ---- ---- make cell ---- ---- */
 static Cell * make_cell (Cell * cell) {
-  Cell * r = malloc(1*sizeof(Cell));
+  Cell * r = malloc( 1 * sizeof(Cell) );
   *r = *cell;
   return r;
 }
 
 static Cell * cell_cons (Cell * cell) {
-  return make_cell(&(Cell){TCONS, .car_ = cell});
+  return make_cell(&(Cell){ TCONS, .car_ = cell });
 }
 static Cell * cell_int (int a) {
-  return make_cell(&(Cell){TINT, .int_=a});
+  return make_cell(&(Cell){ TINT, .int_=a });
 }
 static Cell * cell_symbol (char * a) {
-  Cell * r = make_cell(&(Cell){TSYMBOL, .int_ = 0});
-  r->symbol_ = malloc(sizeof(char) * (strlen(a) + 1) );
+  Cell * r = make_cell(&(Cell){ TSYMBOL, .int_ = 0 });
+  r->symbol_ = malloc( sizeof(char) * (strlen(a)+1) );
   strcpy(r->symbol_, a);
   return r;
 }
-
 /* ---- ---- ---- ---- ---- ---- */
 
 /* ---- ---- lex tools ---- ---- */
@@ -138,9 +137,9 @@ static Cell * set_lambda_args (Cell * name_l_, Cell * v_l_, Cell ** env ) {
   Cell * local_env = *env;
   Cell * name_l;
   Cell * v_l;
-  for (name_l = name_l_, v_l = v_l_;name_l!=Nil&&v_l!=Nil;name_l = name_l->cdr_, v_l = v_l->cdr_) {
-	Cell * A = make_cell(&(Cell){TCONS,.car_=name_l->car_,.cdr_=eval(v_l,env)});
-	Cell * new_env = make_cell(&(Cell){TCONS, .car_=A, .cdr_=local_env});
+  for (name_l = name_l_, v_l = v_l_; name_l!=Nil && v_l!=Nil; name_l = name_l->cdr_, v_l = v_l->cdr_) {
+	Cell * A       = make_cell(&(Cell){ TCONS, .car_=name_l->car_, .cdr_=eval(v_l,env) });
+	Cell * new_env = make_cell(&(Cell){ TCONS, .car_=A,            .cdr_=local_env });
 	local_env = new_env;
   }
   return local_env;
@@ -199,6 +198,7 @@ static Cell * eval (Cell * cell, Cell ** env) {
   return eval(cell->cdr_, env);
 }
 
+// (+ _ ...)
 static inline Cell * plus_eval (Cell * cell, Cell ** env) {
   int result = 0;
   for (Cell * p = cell; p != Nil; p = p->cdr_) {
@@ -210,6 +210,7 @@ static inline Cell * plus_eval (Cell * cell, Cell ** env) {
   }
   return cell_int(result);
 }
+// (- _ ...)
 static inline Cell * minus_eval (Cell * cell, Cell ** env) {
   Cell * p = eval(cell,env);
   int result = p->int_;
@@ -222,6 +223,7 @@ static inline Cell * minus_eval (Cell * cell, Cell ** env) {
   }
   return cell_int(result);
 }
+// (* _ ...)
 static inline Cell * time_eval (Cell * cell, Cell ** env) {
   int result = 1;
   for (Cell * p = cell; p != Nil; p = p->cdr_) {
@@ -237,6 +239,7 @@ static inline Cell * time_eval (Cell * cell, Cell ** env) {
   }
   return cell_int(result);
 }
+// (/ _ ...)
 static inline Cell * divid_eval (Cell * cell, Cell ** env) {
   Cell * p = eval(cell,env);
   int result = p->int_;
@@ -255,6 +258,7 @@ static inline Cell * divid_eval (Cell * cell, Cell ** env) {
   }
   return cell_int(result);
 }
+// (> _ _)
 static inline Cell * great_eval (Cell * cell, Cell ** env) {
   Cell * L = eval(cell,       env);
   Cell * R = eval(cell->cdr_, env);
@@ -263,6 +267,7 @@ static inline Cell * great_eval (Cell * cell, Cell ** env) {
   printf("greater error\n");
   return Nil;
 }
+// (< _ _)
 static inline Cell * less_eval (Cell * cell, Cell ** env) {
   Cell * L = eval(cell,       env);
   Cell * R = eval(cell->cdr_, env);
@@ -271,6 +276,7 @@ static inline Cell * less_eval (Cell * cell, Cell ** env) {
   printf("less error\n");
   return Nil;
 }
+// (= _ _)
 static inline Cell * equal_eval (Cell * cell, Cell ** env) {
   Cell * L = eval(cell,       env);
   Cell * R = eval(cell->cdr_, env);
@@ -279,6 +285,7 @@ static inline Cell * equal_eval (Cell * cell, Cell ** env) {
   printf("equal error\n");
   return Nil;
 }
+// (if _ _ _)
 static inline Cell * if_eval (Cell * cell, Cell ** env) {
   Cell * p = eval(cell,env);
   if (p == TRUE)
@@ -289,12 +296,15 @@ static inline Cell * if_eval (Cell * cell, Cell ** env) {
   printf("if error\n");
   return Nil;
 }
+// (car _)
 static inline Cell * car_eval (Cell * cell, Cell ** env) {
   return cell->car_;
 }
+// (cdr _)
 static inline Cell * cdr_eval (Cell * cell, Cell ** env) {
   return cell->car_->cdr_;
 }
+// (def _ _)
 static inline Cell * def_eval (Cell * cell, Cell ** env) {
   Cell * new_env = cell_cons(cell);
   new_env->car_->cdr_ = eval(cell->cdr_, env);
@@ -302,8 +312,9 @@ static inline Cell * def_eval (Cell * cell, Cell ** env) {
   *env = new_env;
   return new_env;
 }
+// (lambda (_ ...) _)
 static inline Cell * lambda_eval (Cell * cell, Cell ** env) {
-  return make_cell(&(Cell){TFUN, .car_=cell->car_,.cdr_=cell->cdr_});
+  return make_cell(&(Cell){ TFUN, .car_=cell->car_,.cdr_=cell->cdr_ });
 }
 
 /* ==== ==== ==== ==== ==== ==== ==== */
